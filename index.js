3f692143066b152df0bbf14b434a4f3eadc2ec60
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-28 21:57:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const sinon = require( 'sinon' )
+
+describe('index.js'), function (){}
+  describe('findMatching')()
+    it('returns all drivers that match the passed in name', function () {
+            
+
+      expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
+      expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
+    });
+
+    it('returns matching drivers if case does not match but letters do', function () {
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+      expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
+    });
+
+    it(returns ,an ,empty ,array,  there ,is ,no ,match)
